9221e4c3eb2d58d8fd73357e56f18966303d2c07
Daniel Guramulta <daniel.guramulta@analog.com>
2019-07-23 09:18:06 +0300

    Update documentation to e2b4d6d

---
+++ search/functions_1.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['calibrateadc',['calibrateADC',['../group__m2k.html#ga7d524fca63468154cb8594672e41bb1b',1,'libm2k::contexts::M2k']]],
+  ['calibratedac',['calibrateDAC',['../group__m2k.html#gaf94086f5b524e3498c90fda33f93dd32',1,'libm2k::contexts::M2k']]],
+  ['cancelbuffer',['cancelBuffer',['../group__analogin.html#gabbc602ad871cd9ce2a07117e8b6f033e',1,'libm2k::analog::M2kAnalogIn::cancelBuffer()'],['../group__analogout.html#ga40f70e5c88ad3b3662ad9f58111755cf',1,'libm2k::analog::M2kAnalogOut::cancelBuffer()'],['../group__analogout.html#gac300b2bb11c538ec192394b865425969',1,'libm2k::analog::M2kAnalogOut::cancelBuffer(unsigned int chn)']]],
+  ['cancelbufferin',['cancelBufferIn',['../group__digital.html#gaa70d42e03f01ee3249d3f0bce652bc5f',1,'libm2k::digital::M2kDigital']]],
+  ['cancelbufferout',['cancelBufferOut',['../group__digital.html#ga27ec34b08ad73e4d36feb84f98615053',1,'libm2k::digital::M2kDigital']]],
+  ['contextclose',['contextClose',['../group__context.html#gab18606249b33007492a7536531f7be40',1,'libm2k::contexts']]],
+  ['contextcloseall',['contextCloseAll',['../group__context.html#ga69feb343ba3fc612627c7083a2fdaa4e',1,'libm2k::contexts']]],
+  ['convertrawtovolts',['convertRawToVolts',['../group__analogin.html#ga07baadf1372522d0dee601533563b7c9',1,'libm2k::analog::M2kAnalogIn::convertRawToVolts()'],['../group__analogout.html#ga14f6381e42eccf0a557d30971da798d8',1,'libm2k::analog::M2kAnalogOut::convertRawToVolts()']]],
+  ['convertvoltstoraw',['convertVoltsToRaw',['../group__analogin.html#ga1078bb8aa2f78070ff81b34e283701c5',1,'libm2k::analog::M2kAnalogIn::convertVoltsToRaw()'],['../group__analogout.html#ga01508408603bcae62958056c99eea05f',1,'libm2k::analog::M2kAnalogOut::convertVoltsToRaw()']]],
+  ['convvoltstoraw',['convVoltsToRaw',['../group__analogout.html#ga90482c32d66f89e577a5d938ed931572',1,'libm2k::analog::M2kAnalogOut']]]
+];
